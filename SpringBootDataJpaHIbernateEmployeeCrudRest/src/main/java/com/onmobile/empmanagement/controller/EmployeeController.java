2f34a201ddd9537d9d581e8160089bb1a113cab0
rahulranjan95 <106315432+rahulranjan95@users.noreply.github.com>
2022-07-21 13:02:32 +0530

    Rahul url changes

--- SpringBootDataJpaHIbernateEmployeeCrudRest/src/main/java/com/onmobile/empmanagement/controller/EmployeeController.java
+++ SpringBootDataJpaHIbernateEmployeeCrudRest/src/main/java/com/onmobile/empmanagement/controller/EmployeeController.java
@@ -18,7 +18,7 @@ import com.onmobile.empmanagement.model.Employee;
 import com.onmobile.empmanagement.service.EmployeeService;
 
 @RestController
-@RequestMapping("/employees")
+@RequestMapping("/emp")
 public class EmployeeController {
 	@Autowired
 	EmployeeService service;
